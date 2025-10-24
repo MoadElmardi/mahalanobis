5481aed5939299b262b7a43f37c4f502606ea811
MoadElmardi <moad.elmardi@astek.net>
2025-10-24 15:30:43 +0200

    Nettoyage du code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // mod mahalanobis_clair;
-mod mahalanobis_tfhe;
+// mod mahalanobis_tfhe;
 // mod test_arrays;
 
 // use mahalanobis_clair::main as clair_main;
-use mahalanobis_tfhe::main as tfhe_main;
+// use mahalanobis_tfhe::main as tfhe_main;
 //use test_arrays::main as test_arrays_main;
 
 fn main() {
-    tfhe_main();
+    // clair_main();
+    // tfhe_main();
+    // test_arrays_main();
 }
